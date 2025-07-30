2fdd9f0040871e65cd2afa1acd5b414985175902
njenga <nganganjenga14915@gmail.com>
2025-07-30 11:29:59 +0300

    last case-sensitive import path for CategoryCard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Link } from 'react-router-dom';
-import CategoryCard from '../components/CategoryCard';
+import CategoryCard from '../Components/CategoryCard';
 
  // 
 
